7b801677458aa5a95d906197e492ee11a2e512c0
Alexander Borovykh <immaculate.pine@gmail.com>
2014-09-18 18:56:53 +0400

    Customizable redirects in OmniauthCallbacksController

--- app/controllers/superb_auth/omniauth_callbacks_controller.rb
+++ app/controllers/superb_auth/omniauth_callbacks_controller.rb
@@ -15,18 +15,18 @@ class SuperbAuth::OmniauthCallbacksController < Devise::OmniauthCallbacksControl
     alias_method provider, :all
   end
 
-  private
+  protected
 
     # Cases when there is such an identity.
     def authenticate_with_identity
       if user_signed_in?
         # User is already authorized. So, the identity could not be attached to him
-        redirect_to main_app.edit_user_registration_url, alert: I18n.t('omniauth_callbacks.all.identity_alredy_taken')
+        redirect_after_authenticate_with_identity_signed_in
       else
         # User is unauthorized. Authorize him
         flash[:notice] = I18n.t('omniauth_callbacks.all.logged_in')
         sign_in @identity.user
-        redirect_to main_app.root_url
+        redirect_after_authenticate_with_identity
       end
     end
 
@@ -39,18 +39,38 @@ class SuperbAuth::OmniauthCallbacksController < Devise::OmniauthCallbacksControl
         current_user.get_user_info_from_identity(identity, save: true)
         # Sign in user and redirect him to profile
         sign_in current_user
-        redirect_to main_app.edit_user_registration_url, notice: I18n.t('omniauth_callbacks.all.identity_attached')
+        redirect_after_authenticate_without_identity_signed_id
       else
         # Create new user. Then create new identity and attach it to him
         user = User.from_omniauth(@auth)
         user.skip_confirmation! if user.respond_to?(:skip_confirmation!)
         if user.save
           sign_in user
-          redirect_to main_app.root_url, notice: I18n.t('omniauth_callbacks.all.registred_successfully')
+          redirect_after_authenticate_without_identity_success
         else
-          redirect_to main_app.root_url, alert: I18n.t('omniauth_callbacks.all.unknown_error')
+          redirect_after_authenticate_without_identity_failure
         end
       end
+    end
+
+    def redirect_after_authenticate_with_identity_signed_in
+      redirect_to main_app.edit_user_registration_url, alert: I18n.t('omniauth_callbacks.all.identity_alredy_taken')
+    end
+
+    def redirect_after_authenticate_with_identity
+      redirect_to main_app.root_url
+    end
+
+    def redirect_after_authenticate_without_identity_signed_id
+      redirect_to main_app.edit_user_registration_url, notice: I18n.t('omniauth_callbacks.all.identity_attached')
+    end
+
+    def redirect_after_authenticate_without_identity_success
+      redirect_to main_app.root_url, notice: I18n.t('omniauth_callbacks.all.registred_successfully')
+    end
+
+    def redirect_after_authenticate_without_identity_failure
+      redirect_to main_app.root_url, alert: I18n.t('omniauth_callbacks.all.unknown_error')
     end
 
 end
