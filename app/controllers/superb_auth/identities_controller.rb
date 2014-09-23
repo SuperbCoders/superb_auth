5c0cf38a468bf7d87cef74401b5d3c28d5109884
Alexander Borovykh <immaculate.pine@gmail.com>
2014-09-23 16:10:37 +0400

    Customizable redirects in IdentitiesController

--- app/controllers/superb_auth/identities_controller.rb
+++ app/controllers/superb_auth/identities_controller.rb
@@ -7,17 +7,30 @@ class SuperbAuth::IdentitiesController < SuperbAuth::ApplicationController
     if (current_user.email.present? && current_user.encrypted_password.present?) || current_user.identities.count > 1
       destroy_identity
     else
-      redirect_to main_app.edit_user_registration_url, alert: "Чтобы удалить привязку к #{params[:provider]}, укажите email и пароль, либо привяжите другую социальную сеть"
+      redirect_after_could_not_be_destroyed
     end
   end
 
-  private
+  protected
 
     def destroy_identity
       if @identity.try(:destroy)
-        redirect_to main_app.edit_user_registration_url, notice: "Привязка к #{params[:provider]} удалена"
+        redirect_after_destroy_success
       else
-        redirect_to main_app.edit_user_registration_url, alert: "Не удалось удалить привязку к #{params[:provider]}"
+        redirect_after_destroy_fail
       end
     end
+
+    def redirect_after_destroy_success
+      redirect_to main_app.edit_user_registration_url, notice: "Привязка к #{params[:provider]} удалена"
+    end
+
+    def redirect_after_destroy_fail
+      redirect_to main_app.edit_user_registration_url, alert: "Не удалось удалить привязку к #{params[:provider]}"
+    end
+
+    def redirect_after_could_not_be_destroyed
+      redirect_to main_app.edit_user_registration_url, alert: "Чтобы удалить привязку к #{params[:provider]}, укажите email и пароль, либо привяжите другую социальную сеть"
+    end
+
 end
